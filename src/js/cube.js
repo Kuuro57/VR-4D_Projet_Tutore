620922460c594090efeddc7c359544f090d023f9
EliasKho <elias.khodjaoui@free.fr>
2025-12-15 15:25:53 +0100

    feature: Ajout de la classe Cube qui représente un cube en 3D

---
+++ src/js/cube.js
@@ -0,0 +1,42 @@
+import { Sommet } from "./sommet.js";
+import { Arete } from "./arete.js";
+
+/**
+ * Classe représentant un cube en 3D
+ */
+class Cube {
+
+    /**
+     * Liste des sommets définis dans sommet.js
+     */
+    sommets;
+
+    /**
+     * Liste des arêtes définies dans arete.js
+     */
+    aretes;
+
+    /**
+     * Constructeur du cube
+     * @param {Sommet[]} sommetsParameters 
+     * @param {Arete[]} aretesParameters 
+     */
+    constructor(sommetsParameters, aretesParameters) {
+        this.sommets = sommetsParameters;
+        this.aretes = aretesParameters;
+    }
+
+    /**
+     * Methode d'affichage du cube dans la scène
+     * @param {BABYLON.Scene} scene 
+     */
+    build(scene) {
+        sommets.forEach(sommet => {
+            sommet.build(scene);
+        });
+        aretes.forEach(arete => {
+            arete.build(scene);
+        });
+    }
+
+}
