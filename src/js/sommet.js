a328ffa49dec4667dff7c8f87d45decdd925a338
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2025-12-15 15:25:31 +0100

    feature: ajout de la classe sommet qui représente un sommet dans l'espace 3D

--- src/js/sommet.js
+++ src/js/sommet.js
@@ -1,13 +1,51 @@
+/**
+ * Classe représentant un sommet dans un espace 3D
+ */
 class Sommet {
 
-    // TODO
+    /**
+     * @type {String} Nom du sommet
+     */
+    name;
 
-    constructor(parameters) {
-        
-        // TODO
+    /**
+     * @type {BABYLON.Vector3} Position du sommet
+     */
+    vector;
 
+    /**
+     * Constructeur de la classe Sommet
+     * @param {String} n Nom du sommet
+     * @param {BABYLON.Vector3} v Position du sommet
+     */
+    constructor(n, v) {
+        this.vector = v;
+        this.name = n;
     }
 
-    // TODO
+    /**
+     * Méthode qui construit le sommet dans la scène
+     * @param {BABYLON.Scene} scene
+     */
+    build(scene) {
+        const redMat = new BABYLON.StandardMaterial("redMat", scene);
+        redMat.diffuseColor = BABYLON.Color3.Red();
 
+        const sphere = BABYLON.MeshBuilder.CreateSphere(`sphere${name}`, { diameter: 0.2, segments: 32 }, scene);
+        sphere.position.set(this.vector.x, this.vector.y, this.vector.z);
+        sphere.material = redMat;
+    }
+
+    /**
+     * Méthode qui retourne la position du sommet
+     * @returns {BABYLON.Vector3} Position du sommet
+     */
+    getVector() {
+        return this.vector;
+    }
+}
+
+// Export de la classe
+export {
+    Sommet
 }
