6227cd46c2bf60ef43db014215881915ac5d93dd
unknown <noahallart57@gmail.com>
2026-01-07 10:04:05 +0100

    refactor: Optimisation de la mise à jour des faces à chaque frame

--- src/js/faceCarre.js
+++ src/js/faceCarre.js
@@ -65,45 +65,42 @@ class FaceCarre {
         this.sommet4 = s4;
     }
 
+
+
+
+
+
     /**
      * Méthode qui permet de construire la face carrée sur la scène
      * @param {BABYLON.Scene} scene Scène sur laquelle on veut construire la face carrée
      */
     build(scene) {
-
         this.scene = scene;
-
         const mesh = new BABYLON.Mesh(this.name, scene);
 
-        // Matériau
         const mat = new BABYLON.StandardMaterial(`${this.name}_mat`, scene);
         mat.diffuseColor = new BABYLON.Color3(0.8, 0.8, 0.8);
-        mat.alpha = 0.5; // opacité
-        mat.backFaceCulling = false; // = sideOrientation double side (2 cotes visibles)
+        mat.alpha = 0.5;
+        mat.backFaceCulling = false; 
         mesh.material = mat;
 
         this.mesh = mesh;
 
-        this._updateVertexData();
-
+        this.update(true);
     }
+
+
+
+
+
 
     /**
      * Met à jour la position de la face carrée dans l'espace 3D
      */
-    update() {
-
+    update(isFirstTime = false) {
         if (!this.mesh) return;
-        this._updateVertexData();
 
-    }
-
-
-    /**
-     * Mise à jour interne du VertexData
-     */
-    _updateVertexData() {
-
+        // Talbeau des positions de chaque point
         const positions = [
             this.sommet1.vector.x, this.sommet1.vector.y, this.sommet1.vector.z,
             this.sommet2.vector.x, this.sommet2.vector.y, this.sommet2.vector.z,
@@ -111,22 +108,34 @@ class FaceCarre {
             this.sommet4.vector.x, this.sommet4.vector.y, this.sommet4.vector.z,
         ];
 
-        // Deux triangles pour faire le carré
-        const indices = [
-            0, 1, 2, // ◿
-            0, 2, 3 // ◸
-        ];
+        if (isFirstTime) {
 
-        const normals = [];
-        BABYLON.VertexData.ComputeNormals(positions, indices, normals);
+            // Initialisation
+            const indices = [0, 1, 2, 0, 2, 3];
+            const normals = [];
+            BABYLON.VertexData.ComputeNormals(positions, indices, normals);
 
-        const vertexData = new BABYLON.VertexData();
-        vertexData.positions = positions;
-        vertexData.indices = indices;
-        vertexData.normals = normals;
+            const vertexData = new BABYLON.VertexData();
+            vertexData.positions = positions;
+            vertexData.indices = indices;
+            vertexData.normals = normals;
 
-        vertexData.applyToMesh(this.mesh, true);
+            vertexData.applyToMesh(this.mesh, true);
+        } 
+        
+        else {
+
+            // Mise à jour des positions
+            this.mesh.updateVerticesData(BABYLON.VertexBuffer.PositionKind, new Float32Array(positions));
+            
+            // Recalcul des normales (pour la lumière)
+            const normals = [];
+            BABYLON.VertexData.ComputeNormals(positions, [0, 1, 2, 0, 2, 3], normals);
+            this.mesh.updateVerticesData(BABYLON.VertexBuffer.NormalKind, new Float32Array(normals));
+        }
     }
+
+        
 }
 
 
