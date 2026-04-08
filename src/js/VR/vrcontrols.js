c66de2afb78d655596f1e7ba8eea18a2e22595a2
armbrust5u <loup.armbruster7@etu.univ-lorraine.fr>
2026-04-08 11:59:40 +0200

    fix: ambi

--- src/js/VR/vrcontrols.js
+++ src/js/VR/vrcontrols.js
@@ -590,7 +590,7 @@ function attachMenuToHand() {
   globalPanelMesh.position = new BABYLON.Vector3(
     globalMenuSide === "left" ? 0.02 : -0.02,
     0.02,
-    globalMenuSide === "left" ? -0.035 : 0.035
+    globalMenuSide === "left" ? -0.025 : 0.035
   );
   globalPanelMesh.rotation = new BABYLON.Vector3(0, 0, 0);
   globalPanelMesh.setEnabled(true);
